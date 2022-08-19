37a4f661e94337853653222f076592bae54f591e
oraclown <owen.burton13@gmail.com>
2022-08-18 20:16:12 -0500

    Center buttons & text

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -19,13 +19,15 @@ export default function Home() {
         property inspiration
       </h1>
 
-      <main className="flex-1 border border-slate-800 font-serif bg-pink-200 font-family:'Times New Roman'">
-        <div className="p-5">
+      <main className="flex-1 flex flex-col justify-center border border-slate-800 font-serif bg-pink-200 font-family:'Times New Roman'">
+        <div className="text-center p-5">
           <p>nine random fetched pics go here</p>
         </div>
-
-        <button className="p-1 mt-5 ml-5 mr-2.5 border border-slate-800 hover:bg-gradient-to-r from-teal-300 to-amber-300">click for next pics</button>
-        <button className="p-1 mt-5 ml-2.5 mr-5 border border-slate-800 hover:bg-gradient-to-r from-blue-300 to-pink-500">upload image</button>
+        
+        
+        <button className="text-center p-1 mt-5 ml-5 mr-2.5 border border-slate-800 hover:bg-gradient-to-r from-teal-300 to-amber-300">click for next pics</button>
+        <button className="text-center p-1 mt-5 ml-2.5 mr-5 border border-slate-800 hover:bg-gradient-to-r from-blue-300 to-pink-500">upload image</button>
+        
       </main>
 
       <footer className='text-center p-5 border border-slate-800 bg-emerald-200 font-serif font-family:"Times New Roman"'>
