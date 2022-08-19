61900155b2bcd4b83521e7fe94f20b5bb3ffac4c
oraclown <owen.burton13@gmail.com>
2022-08-18 20:34:04 -0500

    Mess w/ colors

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -15,22 +15,22 @@ export default function Home() {
         <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png"/>
       </Head>
 
-      <h1 className="p-5 text-lg text-center border border-slate-800 font-serif bg-red-200 font-family:'Times New Roman'">
+      <h1 className="p-5 text-lg text-center border border-slate-800 font-serif bg-zinc-50 font-family:'Times New Roman'">
         property inspiration
       </h1>
 
-      <main className="flex-1 flex flex-col justify-center border border-slate-800 font-serif bg-pink-200 font-family:'Times New Roman'">
+      <main className="flex-1 flex flex-col justify-center border border-slate-800 font-serif bg-zinc-50 font-family:'Times New Roman'">
         <div className="text-center p-5">
           <p>nine random fetched pics go here</p>
         </div>
         
         <div className="flex justify-center">
-        <button className="text-center p-1 mt-5 ml-5 mr-2.5 border border-slate-800 hover:bg-gradient-to-r from-teal-300 to-amber-300">click for next pics</button>
-        <button className="text-center p-1 mt-5 ml-2.5 mr-5 border border-slate-800 hover:bg-gradient-to-r from-blue-300 to-pink-500">upload image</button>
+        <button className="text-center p-1 mt-5 ml-5 mr-2.5 border border-slate-800 bg-zinc-50 hover:bg-gradient-to-r from-teal-300 to-amber-300">click for next pics</button>
+        <button className="text-center p-1 mt-5 ml-2.5 mr-5 border border-slate-800 bg-zinc-50 hover:bg-gradient-to-r from-blue-300 to-pink-500">upload image</button>
         </div>
       </main>
 
-      <footer className='text-center p-5 border border-slate-800 bg-emerald-200 font-serif font-family:"Times New Roman"'>
+      <footer className='text-center p-5 border border-slate-800 bg-zinc-50 font-serif font-family:"Times New Roman"'>
         <p>
         by owen | < a className="underline text-blue-600 hover:text-blue-800 visited:text-purple-600" href="https://www.twitter.com/oraclown">twitter</a>
         &nbsp;| < a className="underline text-blue-600 hover:text-blue-800 visited:text-purple-600" href="https://www.github.com/oraclown">github</a>
